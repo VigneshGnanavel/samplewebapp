076da7ec2547e41127da53cd0d7fe538777293ad
Vignesh Gnanavel <prathvikvignesh@gmail.com>
2024-07-12 11:05:32 +0530

    changed output

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('hi today friday in key sparks!');
+  res.send('hi today friday in key sparks and time is 11.30!');
 });
 
 app.listen(port, () => {
